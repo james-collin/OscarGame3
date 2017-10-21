b3548545f02a707119a4365cd391cd380c695862
James Collin <jamescollin1992@gmail.com>
2017-10-21 19:51:21 +0530

    first commit

---
+++ Project4/Invader.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Inc.h"
+
+class Invader
+{
+public:
+	Sprite shape;
+
+	int HP;
+	int HPMax;
+	bool leftWall;
+	bool rightWall;
+	bool leftSide;
+	//std::vector<Bullet> bullets;
+
+	Invader(Texture *texture, Vector2u windowSize)
+	{
+		this->HPMax = rand() % 3 + 1;
+		this->HP = this->HPMax;
+		this->leftWall = false;
+		this->rightWall = false;
+		this->shape.setTexture(*texture);
+
+		this->shape.setScale(1.5f, 1.5f);
+
+		this->shape.setPosition((windowSize.y, rand() % int(windowSize.x - this->shape.getGlobalBounds().width)), -60.f);
+	}
+
+
+
+	~Invader() {}
+};
